3ed4d7aae13a55bccca4630be9f74bbed9552f19
Gilian Joosen <gilian_joosen@hotmail.com>
2016-05-17 16:49:12 +0200

    added Gamecontroller

---
+++ Project3D/Controls/GameController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gilian Joosen on 17/05/16.
+//
+
+#include "GameController.h"
---
+++ Project3D/Controls/GameController.h
@@ -0,0 +1,14 @@
+//
+// Created by Gilian Joosen on 17/05/16.
+//
+
+#ifndef MENGHINDAR_DIRI_GAMECONTROLLER_H
+#define MENGHINDAR_DIRI_GAMECONTROLLER_H
+
+
+class GameController {
+
+};
+
+
+#endif //MENGHINDAR_DIRI_GAMECONTROLLER_H
