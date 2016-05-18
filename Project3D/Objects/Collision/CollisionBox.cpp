2c1feaf2832b870e03ab1d11869b5b4edd172d05
Gilian Joosen <gilian_joosen@hotmail.com>
2016-05-18 11:41:01 +0200

    collision box class added

---
+++ Project3D/Objects/Collision/CollisionBox.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Gilian Joosen on 18/05/16.
+//
+
+#include "CollisionBox.h"
+
+CollisionBox::CollisionBox(float x, float y, float width, float height){
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	this->width = width;
+	this->length = length;
+	this->height = height;
+}
+
+bool CollisionBox::intersect(CollisionBox *collisionBox){
+	float xMin = x-width/2;
+	float xMax = xMin + width;
+
+	float yMin = y-length/2;
+	float yMax = yMin + length;
+
+	float zMin = z - height/2;
+	float zMax = z + height;
+
+	float xMin2 = collisionBox->x-collisionBox->width/2;
+	float xMax2 = xMin2 + collisionBox->width;
+
+	float yMin2 = collisionBox->y - collisionBox->length/2;
+	float yMax2 = yMax2 + collisionBox->length;
+
+	float zMin2 = collisionBox->z - collisionBox->height/2;
+	float zMax2 = zMin2 + collisionBox->height;
+
+	return (xMin <= xMax2 && xMax >= xMin2) &&
+		   (yMin <= yMax2 && yMax >= yMin2) &&
+		   (zMin <= zMax2 && zMax >= zMin2);
+
+}
